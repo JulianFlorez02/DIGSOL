ae9ea3b53240095f72bd2d42cf6dedbdc3b22a92
Julian Florez <u20171157751@usco.edu.co>
2022-09-05 21:22:32 -0500

    Formulario Registrar mecanico

--- VehiTrans.App.Frontend/Program.cs
+++ VehiTrans.App.Frontend/Program.cs
@@ -1,7 +1,10 @@
+//using HolaWeb.App.Persistencia.AppRepositorios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//builder.Services.AddSingleton <IRepositorioPersona, RepositorioPersona>();
 
 var app = builder.Build();
 

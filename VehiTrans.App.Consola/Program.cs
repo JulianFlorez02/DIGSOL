1169bc177509b928e29b19f08ecb2c65ab646c95
JulioCampos23 <camposlizarazo@gmail.com>
2022-09-11 22:39:52 -0500

    JC Backend 2022-09-11

--- VehiTrans.App.Consola/Program.cs
+++ VehiTrans.App.Consola/Program.cs
@@ -8,13 +8,18 @@ namespace VehiTrans.App.Consola
     {
       /* Referencia a RepositorioMecanico a traves de la interfaz*/
         private static IRepositorioMecanico _repoMecanico= new RepositorioMecanico(new Persistencia.AppContext());
+        private static IRepositorioVehiculoTipo _repoVehiculoTipos= new RepositorioVehiculoTipo(new Persistencia.AppContext());
+        private static IRepositorioVehiculo _repoVehiculo= new RepositorioVehiculo(new Persistencia.AppContext());
 
         static void Main(string[] args)
 
         {
             Console.WriteLine("Hello, World!");
-            AddMecanico();
+            //AddMecanico();
             //BuscarMecanico(1);
+            //AddVehiculosTipo("Buseta");
+            //AddVehiculosTipo("Microbus");
+            AddVehiculo();
         }
         private static void AddMecanico()
         {
@@ -31,6 +36,42 @@ namespace VehiTrans.App.Consola
             _repoMecanico.AddMecanico(Mecanico);
 
         }
+
+        private static void AddVehiculosTipo(String VVTipo)
+        {
+            var VVehiculosTipo = new VehiculoTipo
+            {
+                Descripcion = VVTipo,
+            };
+
+            // Console.WriteLine("Inicio");
+            _repoVehiculoTipos.AddVehiculoTipo(VVehiculosTipo);
+            // Console.WriteLine("Sale");
+
+        }
+
+        private static void AddVehiculo()
+        {
+            var VVehiculo = new Vehiculo
+            {
+                Placa = "CUC123",
+                Marca = "Toyota",
+                Modelo = 2021,
+                VehiculoTipoId = 1,
+                CantidadPasajeros = 30,
+                CilindrajeMotor = 3000,
+                PropietarioId = 1,
+                ConductorId = 1,
+                MecanicoId = 1,
+            
+            };
+
+            Console.WriteLine("Inicio V");
+            _repoVehiculo.AddVehiculo(VVehiculo);
+            Console.WriteLine("Sale V");
+
+        }
+
 
         private static void BuscarMecanico(int idMecanico)
         {
--- VehiTrans.App.Dominio/Entidades/Vehiculo.cs
+++ VehiTrans.App.Dominio/Entidades/Vehiculo.cs
@@ -6,14 +6,17 @@ namespace VehiTrans.App.Dominio
     public class Vehiculo
     {
         public int VehiculoId  { get; set; }
-        [MaxLength(7),MinLength(1)]
-        public string? Placa { get; set; }
-        [MaxLength(30),MinLength(1)]
-        public string? Marca { get; set; }
-        [MaxLength(30),MinLength(1)]
-        public string? Modelo { get; set; }
+        [Required]
+        //[MaxLength(7),MinLength(1)]
+        public string Placa { get; set; }
+        [Required]
+        //[MaxLength(30),MinLength(1)]
+        public string Marca { get; set; }
+        //[MaxLength(30),MinLength(1)]
+        public int Modelo { get; set; }
         public int VehiculoTipoId  { get; set; }
         public VehiculoTipo? VehiculoTipo { get; set; }
+        [Required]
         public int CantidadPasajeros { get; set; }
         public int CilindrajeMotor { get; set; }
         public int PropietarioId { get; set; }
